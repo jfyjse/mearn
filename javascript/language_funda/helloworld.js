11ea30c4b1cbfcb9a3aa1b359d11acf45ccb8915
doug <joffy.jose.jfy@gmail.com>
2021-02-04 18:58:50 +0530

    js 1st class resume, luminar site

---
+++ javascript/language_funda/helloworld.js
@@ -0,0 +1,38 @@
+var name="luminartechnolab";
+var num=2;
+// 10234 |0010|
+
+var num1=1;
+var num2=2;
+var temp=0;
+
+
+
+console.log(name +" is " + num + " years old\n");
+
+console.log("num 1 is "+num1 + " num2 is " + num2)
+temp=num1;
+num1=num2;
+num2=temp;
+
+console.log("swapped \n num1= " + num1 +" num2 is "+ num2);
+
+num1=num2+num1;
+num2=num1-num2;
+num1=num1-num2;
+console.log("swapped \n num1= " + num1 +" num2 is "+ num2);
+
+
+
+// different types
+var nu=10;//integer-number
+var name="jinjin";//string
+var avg=10.5;//float-number
+var isAllow= true;//boolean
+
+console.log(typeof(nu));
+console.log(typeof(name));
+console.log(typeof(avg));
+console.log(typeof(isAllow));
+
+
